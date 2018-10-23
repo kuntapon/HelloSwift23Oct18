89964ccdc6370a862241fa2ac512bbbbc3554cd4
mrkun <kuntapon55@gmail.com>
2018-10-23 10:34:04 +0700

    Start App

---
+++ HelloSwift_23oct18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift_23oct18
+//
+//  Created by Kuntapon Tanikkool on 23/10/2561 BE.
+//  Copyright © 2561 Kuntapon Tanikkool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Function
+
+
+}   //Main Class
+
